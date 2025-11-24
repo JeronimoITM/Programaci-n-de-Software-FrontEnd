57bb357a94ec3f1627106a6259630ff17d417605
Jeronimo <castrillonj041@gmail.com>
2025-11-24 18:54:55 -0500

    Agregando fechas y parte del html

--- FrontProyecto/src/main/java/FrontTiendaRopa/FrontProyecto/DTOs/VentaCreateDTO.java
+++ FrontProyecto/src/main/java/FrontTiendaRopa/FrontProyecto/DTOs/VentaCreateDTO.java
@@ -1,7 +1,10 @@
 package FrontTiendaRopa.FrontProyecto.DTOs;
 
+import java.time.LocalDate;
+
 public record VentaCreateDTO(
         Integer clienteId,
         Integer productoId,
-        Integer cantidad
+        Integer cantidad,
+        LocalDate fechaVenta
 ) {}
